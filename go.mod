c85206831716d832d395a3672724fe0f99e9919b
costa86 <costa86@pm.me>
2023-11-24 22:30:51 +0000

    all

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/costa86/copier
+
+go 1.21.1
+
+require (
+	github.com/pkg/sftp v1.13.6
+	github.com/spf13/cobra v1.8.0
+	golang.org/x/crypto v0.15.0
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pkg/sftp"
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
+)
+
+var (
+	host     string
+	port     int
+	username string
+	password string
+	srcFile  string
+	destFile string
+	showHelp bool
+	version  string = "1.0.0"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "copier",
+	Short: fmt.Sprintf("Upload a file to an SFTP server. Version %s", version),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		config := &ssh.ClientConfig{
+			User: username,
+			Auth: []ssh.AuthMethod{
+				ssh.Password(password),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		}
+
+		client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), config)
+		if err != nil {
+			log.Fatal("Failed to dial:", err)
+		}
+		defer client.Close()
+
+		sftpClient, err := sftp.NewClient(client)
+		if err != nil {
+			log.Fatal("Failed to create SFTP client:", err)
+		}
+		defer sftpClient.Close()
+
+		localFile, err := os.Open(srcFile)
+		if err != nil {
+			log.Fatal("Failed to open local file:", err)
+		}
+		defer localFile.Close()
+
+		remoteFile, err := sftpClient.Create(destFile)
+		if err != nil {
+			log.Fatal("Failed to create remote file:", err)
+		}
+		defer remoteFile.Close()
+
+		_, err = io.Copy(remoteFile, localFile)
+		if err != nil {
+			log.Fatal("Failed to copy file contents:", err)
+		}
+
+		fmt.Printf("File '%s' sent to '%s'", srcFile, destFile)
+		os.Exit(0)
+	},
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&host, "host", "T", "", "SSH server host")
+	rootCmd.Flags().IntVarP(&port, "port", "P", 22, "SSH server port")
+	rootCmd.Flags().StringVarP(&username, "username", "U", "", "SSH username")
+	rootCmd.Flags().StringVarP(&password, "password", "W", "", "SSH password")
+	rootCmd.Flags().StringVarP(&srcFile, "src", "S", "", "Source file path")
+	rootCmd.Flags().StringVarP(&destFile, "dest", "D", "", "Destination file path on the server")
+	rootCmd.Flags().BoolVarP(&showHelp, "help", "H", false, "Show help message")
+
+	rootCmd.MarkFlagRequired("host")
+	rootCmd.MarkFlagRequired("username")
+	rootCmd.MarkFlagRequired("password")
+	rootCmd.MarkFlagRequired("src")
+	rootCmd.MarkFlagRequired("dest")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
