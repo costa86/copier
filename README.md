142e22ab78950993180ed813080188f5dc29bca9
costa86 <costa86@pm.me>
2023-11-26 18:41:21 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # copier
+
+    Upload a file to an SFTP server
+    Version 1.0.0
+
+    Usage:
+    copier [flags]
+
+    Flags:
+    -d, --dest string       Destination file path on the server
+    -h, --help              help for copier
+    -t, --host string       SSH server host
+    -w, --password string   SSH password
+    -p, --port int          SSH server port (default 22)
+    -s, --src string        Source file path
+    -u, --username string   SSH username
--- main.go
+++ main.go
@@ -18,13 +18,19 @@ var (
 	password string
 	srcFile  string
 	destFile string
-	showHelp bool
 	version  string = "1.0.0"
 )
 
+func handleFailure(e error) {
+	if e != nil {
+		log.Fatal(e)
+		os.Exit(1)
+	}
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "copier",
-	Short: fmt.Sprintf("Upload a file to an SFTP server. Version %s", version),
+	Short: fmt.Sprintf("Upload a file via SFTP to a remote server\nDeveloped by PCA team\nVersion %s", version),
 	Run: func(cmd *cobra.Command, args []string) {
 
 		config := &ssh.ClientConfig{
@@ -36,33 +42,32 @@ var rootCmd = &cobra.Command{
 		}
 
 		client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), config)
-		if err != nil {
-			log.Fatal("Failed to dial:", err)
-		}
+
+		handleFailure(err)
+
 		defer client.Close()
 
 		sftpClient, err := sftp.NewClient(client)
-		if err != nil {
-			log.Fatal("Failed to create SFTP client:", err)
-		}
+
+		handleFailure(err)
+
 		defer sftpClient.Close()
 
 		localFile, err := os.Open(srcFile)
-		if err != nil {
-			log.Fatal("Failed to open local file:", err)
-		}
+
+		handleFailure(err)
+
 		defer localFile.Close()
 
 		remoteFile, err := sftpClient.Create(destFile)
-		if err != nil {
-			log.Fatal("Failed to create remote file:", err)
-		}
+
+		handleFailure(err)
+
 		defer remoteFile.Close()
 
 		_, err = io.Copy(remoteFile, localFile)
-		if err != nil {
-			log.Fatal("Failed to copy file contents:", err)
-		}
+
+		handleFailure(err)
 
 		fmt.Printf("File '%s' sent to '%s'", srcFile, destFile)
 		os.Exit(0)
@@ -70,13 +75,12 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&host, "host", "T", "", "SSH server host")
-	rootCmd.Flags().IntVarP(&port, "port", "P", 22, "SSH server port")
-	rootCmd.Flags().StringVarP(&username, "username", "U", "", "SSH username")
-	rootCmd.Flags().StringVarP(&password, "password", "W", "", "SSH password")
-	rootCmd.Flags().StringVarP(&srcFile, "src", "S", "", "Source file path")
-	rootCmd.Flags().StringVarP(&destFile, "dest", "D", "", "Destination file path on the server")
-	rootCmd.Flags().BoolVarP(&showHelp, "help", "H", false, "Show help message")
+	rootCmd.Flags().StringVarP(&host, "host", "t", "", "SSH server host")
+	rootCmd.Flags().IntVarP(&port, "port", "p", 22, "SSH server port")
+	rootCmd.Flags().StringVarP(&username, "username", "u", "", "SSH username")
+	rootCmd.Flags().StringVarP(&password, "password", "w", "", "SSH password")
+	rootCmd.Flags().StringVarP(&srcFile, "src", "s", "", "Source file path")
+	rootCmd.Flags().StringVarP(&destFile, "dest", "d", "", "Destination file path on the server")
 
 	rootCmd.MarkFlagRequired("host")
 	rootCmd.MarkFlagRequired("username")
@@ -87,6 +91,6 @@ func init() {
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		handleFailure(err)
 	}
 }
